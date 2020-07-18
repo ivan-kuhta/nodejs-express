f16b06314c26b455fc0c09842d3cdb3bcd96ad4b
Ivan <Ivan1998pi22342@ukr.net>
2020-07-18 16:13:57 +0300

    Add test route && Add express-handlebars

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+
+app.get('/', (request, response, next) => {
+    response.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (request, response, next) => {
+    response.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
 
 
 const PORT = process.env.PORT || 3000;
