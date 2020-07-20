4e0d56f48e89c04edbd377d3b52e961228c275bc
Ivan <Ivan1998pi22342@ukr.net>
2020-07-20 17:51:40 +0300

    Add delete && add remove function

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars');
 const homeRoutes = require('./routes/home');
 const coursesRoutes = require('./routes/courses');
 const addRoutes = require('./routes/add');
+const cardRoutes = require('./routes/card');
 
 const app = express();
 
@@ -17,12 +18,13 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}))
 
 app.use('/', homeRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/add', addRoutes)
+app.use('/card', cardRoutes)
 
 
 const PORT = process.env.PORT || 3000;
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,45 @@
-document.querySelectorAll('.price').forEach(item => {
-    item.textContent = new Intl.NumberFormat('ru-RU', {
+const toCurrency = price => {
+    return new Intl.NumberFormat('ru-RU', {
         currency: 'UAH',
         style: 'currency'
-    }).format(item.textContent)
+    }).format(price)
+}
+
+document.querySelectorAll('.price').forEach(item => {
+    item.textContent = toCurrency(item.textContent)
 })
+
+const $card = document.querySelector('#card');
+
+if($card){
+    $card.addEventListener('click', event => {
+        if(event.target.classList.contains('js-remove')){
+            const id = event.target.dataset.id;
+            console.log(id);
+
+            fetch('/card/remove/' + id, {
+                method: 'delete'
+            }).then(res => res.json()).then(card => {
+                // console.log(card)
+                if(card.courses.length) {
+                    const html = card.courses.map(c => {
+                        return `
+                        <tr>
+                            <td>${c.title}</td>
+                            <td>${c.count}</td>
+                            <td>
+                                <button class="btn btn-small js-remove" data-id=${c.id}>Видалити</button>
+                            </td>
+                        </tr>
+                        `
+                    }).join('')
+                    $card.querySelector('tbody').innerHTML = html
+                    $card.querySelector('.price').textContent = toCurrency(card.price)
+                    
+                } else {
+                    $card.innerHTML = '<p>Корзина пуста</p>'
+                }
+            })
+        }
+    })
+}
