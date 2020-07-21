ad8ec3a4b5631528e86b214880ed53cad2036e42
Ivan <Ivan1998pi22342@ukr.net>
2020-07-21 15:21:56 +0300

    Add Cart DB

--- routes/card.js
+++ routes/card.js
@@ -1,21 +1,23 @@
 const {Router} = require('express');
-const Card = require('../models/card');
+// const Card = require('../models/card');
 const Course = require('../models/course');
 const router = Router();
 
 router.post('/add', async (req, res) => {
-    const course = await Course.getById(req.body.id)
-    await Card.add(course)
+    const course = await Course.findById(req.body.id)
+    await req.user.addToCart(course);
+    // await Card.add(course)
     res.redirect('/card')
 })
 
 router.get('/', async (req, res) => {
-    const card = await Card.fetch();
-    res.render('card', {
-        title: 'Корзина',
-        isCard: true,
-        ...card
-    })
+    // const card = await Card.fetch();
+    // res.render('card', {
+    //     title: 'Корзина',
+    //     isCard: true,
+    //     ...card
+    // })
+    res.json({text:true});
 })
 
 router.delete('/remove/:id', async(req, res) => {
